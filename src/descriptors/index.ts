4444513e31007d46851fa29210baf7b78bee50f0
knownout <a.slavinskii@icloud.com>
2025-02-20 14:24:06 +0300

    Remove descriptor leftovers

--- src/descriptors/index.ts
+++ src/descriptors/index.ts
@@ -1,9 +1,5 @@
-import { tonWalletDescriptors } from "./ton"
-import { tronWalletDescriptors } from "./tron"
 import { ethereumWalletDescriptors } from "./ethereum"
 
 export {
-  tonWalletDescriptors,
-  tronWalletDescriptors,
   ethereumWalletDescriptors
 }
